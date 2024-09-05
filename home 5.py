972f1033258eb5e2bf47de9abf0f4139dac5cfde
SergeyStepanov123 <serezha-stepanov-91@internet.ru>
2024-09-05 08:21:24 +0300

    Add files via upload

---
+++ home 5.py
@@ -0,0 +1,9 @@
+immutable_var = ('er', 45)
+print(immutable_var)
+#immutable_var[1] = 36
+#print(immutable_var)
+#Кортедж изменять нельзя.
+
+mutable_list = ['car', 'size']
+mutable_list[1] = 'long'
+print(mutable_list)
